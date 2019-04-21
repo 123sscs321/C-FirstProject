58ee962a36d29fc6ec5c6a8f374a6dd5a21e821f
Jack Ge <246668653@qq.com>
2019-04-21 23:25:49 +0800

    Title:Create Documents directory.     Detail:1、Add personal resume.            2、Update the modified files.

--- First Project/first.c
+++ First Project/first.c
@@ -199,3 +199,16 @@
 //    system("pause");
 //}
 //
+
+
+//#include <stdlib.h>
+//
+//int main()
+//{
+//    srand(2);
+//    for (int i=0; i<10; i++)
+//    {
+//        printf("%d ", rand()%10);
+//    }
+//    return 0;
+//}
--- First Project/main.c
+++ First Project/main.c
@@ -1,11 +1,26 @@
-#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int strcmp_demo(char str0[],char str1[])
+{
+    int i,result;
+    for (i=0; (result=str0[i]-str1[i])==0; i++)
+    {
+        if (str0[i]=='\0'||str1[i]=='\0')
+        {
+            break;
+        }
+    }
+    printf("i=%d\n",i);
+    return result;
+}
 
 int main()
 {
-    srand(2);
-    for (int i=0; i<10; i++)
-    {
-        printf("%d ", rand()%10);
-    }
+    char str1[]="http://www.caidu.com";
+    char str2[]="http://www.baidu.com";
+    //int result=strcmp(str1,str2);
+    int result=strcmp_demo(str1,str2);
+    printf("str1 - str2=%d\n",result);
     return 0;
 }
